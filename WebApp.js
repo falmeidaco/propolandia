147f9654eb8bd913a80bcad5368863c1f2a592d4
Felipe Almeida <felipealmeidacosta@gmail.com>
2016-05-29 23:14:49 -0300

    criando UI

--- WebApp.js
+++ WebApp.js
@@ -1,13 +1,51 @@
 (function($) {
     
-    var app, WebApp = function() {
-        this.init = function() {
-            //Iniciando o aplicativo.
-            console.log("Olá mundo!")/
+    var WebApp = {
+        
+        screen: {
+            home: {
+                animations:{
+                    revelOptions:function() {                       
+                        $(".app-logo").animate({"top":"30px"},1000);
+                        $(".home-options-default").animate({"right":"15px"},1000);            
+                        $(".home-welcome-text").animate({"bottom":"15px"},1000);
+                    },
+                    hideOptions:function() {
+                        $(".app-logo").animate({"top":"-200px"},1000);
+                        $(".home-options-default").animate({"right":"-300px"},1000);
+                        $(".home-welcome-text").animate({"bottom":"-40px"},1000);
+                    }
+                },
+                init: function() {
+                    
+                }
+            },
+            stage:{
+                animations:{
+                    revelPanel:function() {
+                        $(".screen-stage").animate({"top":"0"},1000);
+                    },
+                    hidePanel:function() {
+                        $(".screen-stage").animate({"top":"-560"},1000);
+                    }
+                }
+            }
+        },
+        init: function () {
+            $(".stage-item a").click(function() {
+                WebApp.screen.home.animations.hideOptions();
+                WebApp.screen.stage.animations.revelPanel();
+            });
+            $(".back-home").click(function() {
+                
+                WebApp.screen.stage.animations.hidePanel();
+                WebApp.screen.home.animations.revelOptions();
+            });
         }
     }
     
-    app = new WebApp();
-    app.init();
+    window.WebApp = WebApp;
     
+    WebApp.init();
+            
 }(jQuery));
