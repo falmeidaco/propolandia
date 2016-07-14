2471e0a95fb771d8c9ebab7e009980d052182a28
Felipe Almeida <felipealmeidacosta@gmail.com>
2016-07-14 14:52:31 -0300

    quase pronto

--- Propolandia.js
+++ Propolandia.js
@@ -4,7 +4,7 @@
     /* Objeto Propolândia */
     var Propolandia = window.Propolandia = function () {
         
-        var stages, current_stage, $this = this;
+        var stages, current_stage, current_stage_step = 1, current_stage_situation = 1, $this = this;
         
         this.setStages = function (stages_data) {
             stages = stages_data;
@@ -30,8 +30,31 @@
             return asset;
         };
         
+        this.getAsset = function (asset_name) {
+            var i, asset = null;
+            for (i = 0; i < current_stage.assets.length; i += 1) {
+                if (current_stage.assets[i].name === asset_name) {
+                    asset = current_stage.assets[i];
+                    break;
+                }
+            }
+            return asset;
+        };
+        
         this.setCurrentStage = function (stage_name){
             current_stage = stages[stage_name];
+        };
+        
+        this.getStageStep = function () {
+            return current_stage_step; 
+        };
+        
+        this.setStageStep = function(step) {
+            current_stage_step = step;
+        };
+        
+        this.getStageSituation = function () {
+            return current_stage_situation; 
         };
         
         this.getCurrentStage = function () {
@@ -51,37 +74,81 @@
                 "name": "icone_padrao",
                 "title": "Ícone padrão",
                 "type": "image/png",
-                "src": "imagens/main-icon.png"
+                "src": "ambientes/supermercado/imagens/main-icon.png"
             },
             {
                 "name": "situacao1_item1",
                 "title": "Arroz",
                 "type": "image/png",
-                "src": "imagens/pacote.png"
+                "src": "ambientes/supermercado/imagens/pacote.png"
             },
             {
                 "name": "situacao1_item2",
                 "title": "Valor",
                 "type": "image/png",
-                "src": "imagens/moeda.png"
-            }
+                "src": "ambientes/supermercado/imagens/moeda.png"
+            },
+            {
+                "name": "slide_1_2",
+                "title": "Slide 1 - 2",
+                "type": "image/jpg",
+                "src": "ambientes/supermercado/imagens/slide_1_2.jpg"
+            } 
         ],
         "properties": {
-            "name": "Supermercado",
+            "path": "ambientes/supermercado/",
+            "name": "Mercadinho",
             "nicename": "supermercado",
             "welcome_prefix": "Você está no ",
             "description": "",
             "icon_default": "icone_padrao",
             "icon_hover": "icone_padrao"
         },
+        "story_steps":[
+            {
+                "type":"slide",
+                "content":[
+                    {
+                        "image_asset":"slide_1_2",
+                        "text":"<span>DONA LUZIA</span> - Joãozinho meu filho, como vai você?<br><span>Joãozinho</span> - Estou bem Dona Luzia, e a senhora, indo para as compras?"
+                    },
+                    {
+                        "image_asset":"slide_1_2",
+                        "text":"<span>DONA LUZIA</span> - Sim sim meu querido, mas eu estou tão cansada... Poderia me ajudar a fazer umas contas? Você é bom em matemática né meu jovem?<br><span>Joãozinho</span> - Mas é claro Dona Luzia!"
+                    },
+                    {
+                        "image_asset":"noimage",
+                        "text":"Ajude Dona Luzia a fazer corretamente as contas."
+                    }
+                ]
+            },
+            {
+                "type":"situation",
+                "content":1
+            },
+            {
+                "type":"slide",
+                "content":[
+                    {
+                        "image_asset":"noimage",
+                        "text":"Parabéns! Você acertou todas as questões!"
+                    }
+                ]
+            },
+            {
+                "type":"end",
+                "content":null
+            }
+        ],
         "situations": [
             {
                 "name": "Arroz",
-                "title": "Comprar 4kg de arroz",
-                "description": "Você precisa comprar <strong>4kg de arroz</strong>. Cada pacote de arroz tem <strong>1kg</strong>. Cada kg de arroz custa <strong>R$ 3,00</strong>. Complete a tabela com o valor correspondente à quantidade de pacotes de arroz.",
-                "table_size": 2,
+                "title": "Comprar Arroz",
+                "description": "Cada pacote de arroz tem <strong>1kg</strong>. Cada kg de arroz custa <strong>R$ 3,00</strong>. Complete a tabela com o valor correspondente à quantidade de pacotes de arroz.",
+                "table_values": [2, 3, 5, 10],
+                "table_values_answers": [6, 9, 15, 30],
                 "item_1": {
-                    "icon_default": "situacao1_icone1",
+                    "icon_default": "situacao1_item1",
                     "name": "Arroz",
                     "unit": "kg",
                     "start_value": 1,
@@ -90,13 +157,13 @@
                     "relation": 3
                 },
                 "item_2": {
-                    "icon_default": "situacao1_icone2",
+                    "icon_default": "situacao1_item2",
                     "name": "Moeda",
                     "unit": "R$",
                     "start_value": 3,
                     "min_value": 1,
                     "max_value": 15,
-                    "relation": 0.33
+                    "relation": 0.3
                 },
                 "stars": 1
             }
