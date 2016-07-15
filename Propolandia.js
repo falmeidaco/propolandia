62a3770a4d87fb10d74d679c1c548039250623a5
Felipe Almeida <felipealmeidacosta@gmail.com>
2016-07-15 14:21:41 -0300

    correções e novas situações

--- Propolandia.js
+++ Propolandia.js
@@ -57,6 +57,10 @@
             return current_stage_situation; 
         };
         
+        this.setStageSituation = function (situation_id) {
+            current_stage_situation = situation_id; 
+        };
+        
         this.getCurrentStage = function () {
             return current_stage;  
         };
@@ -71,6 +75,30 @@
         "description": "Ambiente supermercado",
         "assets": [
             {
+                "name": "icon_iorgute",
+                "title": "Iorgute",
+                "type": "image/png",
+                "src": "ambientes/supermercado/imagens/icon_iorgute.png"
+            },
+			{
+                "name": "icon_arroz",
+                "title": "Iorgute",
+                "type": "image/png",
+                "src": "ambientes/supermercado/imagens/icon_arroz.png"
+            },
+			{
+                "name": "icon_moeda",
+                "title": "Iorgute",
+                "type": "image/png",
+                "src": "ambientes/supermercado/imagens/icon_moeda.png"
+            },
+			{
+                "name": "icon_suco",
+                "title": "Suco",
+                "type": "image/png",
+                "src": "ambientes/supermercado/imagens/icon_suco.png"
+            },
+			{
                 "name": "icone_padrao",
                 "title": "Ícone padrão",
                 "type": "image/png",
@@ -126,12 +154,52 @@
                 "type":"situation",
                 "content":1
             },
+			{
+                "type":"slide",
+                "content":[
+                    {
+                        "image_asset":"noimage",
+                        "text":"Parabéns! Você acertou todas as questões! <br />Agora vamos ver se você consegue resolver a próxima situação."
+                    }
+                ]
+            },
+			{
+                "type":"situation",
+                "content":2
+            },
+            {
+                "type":"slide",
+                "content":[
+                    {
+                        "image_asset":"noimage",
+                        "text":"Parabéns! Você acertou todas as questões! <br />Agora vamos ver se você consegue resolver a próxima situação."
+                    }
+                ]
+            },
+			{
+                "type":"situation",
+                "content":3
+            },
             {
                 "type":"slide",
                 "content":[
                     {
                         "image_asset":"noimage",
                         "text":"Parabéns! Você acertou todas as questões!"
+                    }
+                ]
+            }
+            ,
+			{
+                "type":"slide",
+                "content":[
+                     {
+                        "image_asset":"slide_1_2",
+                        "text":"<span>DONA LUZIA</span> - Muito obrigado Joãozinho! Você é muito esperto!<br><span>Joãozinho</span> - Estou bem Dona Luzia, e a senhora, indo para as compras?"
+                    },
+                    {
+                        "image_asset":"noimage",
+                        "text":"Agora que você já aprender a calcular o valor das compras do mercadinho, explore os outros lugares da cidade!"
                     }
                 ]
             },
@@ -142,13 +210,39 @@
         ],
         "situations": [
             {
+                "name": "Suco",
+                "title": "Comprar Suco",
+                "description": "Cada garrafa de suco tem 3 litros <strong>(l)</strong>. Cada garrafa custa <strong>R$ 5,00</strong>. Complete a tabela com o valor correspondente à quantidade de garrafas de suco.",
+                "table_values": [1, 3, 7, 4],
+                "table_values_answers": [5, 15, 35, 20],
+                "item_1": {
+                    "icon_default": "icon_suco",
+                    "name": "Suco",
+                    "unit": "l",
+                    "start_value": 1,
+                    "min_value": 1,
+                    "max_value": 5,
+                    "relation": 5
+                },
+                "item_2": {
+                    "icon_default": "icon_moeda",
+                    "name": "Moeda",
+                    "unit": "R$",
+                    "start_value": 5,
+                    "min_value": 1,
+                    "max_value": 30,
+                    "relation": 0.2
+                },
+                "stars": 1
+            },
+            {
                 "name": "Arroz",
                 "title": "Comprar Arroz",
                 "description": "Cada pacote de arroz tem <strong>1kg</strong>. Cada kg de arroz custa <strong>R$ 3,00</strong>. Complete a tabela com o valor correspondente à quantidade de pacotes de arroz.",
                 "table_values": [2, 3, 5, 10],
                 "table_values_answers": [6, 9, 15, 30],
                 "item_1": {
-                    "icon_default": "situacao1_item1",
+                    "icon_default": "icon_arroz",
                     "name": "Arroz",
                     "unit": "kg",
                     "start_value": 1,
@@ -157,13 +251,39 @@
                     "relation": 3
                 },
                 "item_2": {
-                    "icon_default": "situacao1_item2",
+                    "icon_default": "icon_moeda",
                     "name": "Moeda",
                     "unit": "R$",
                     "start_value": 3,
                     "min_value": 1,
                     "max_value": 15,
                     "relation": 0.3
+                },
+                "stars": 1
+            },
+            {
+                "name": "Iorgute",
+                "title": "Comprar Iorgute",
+                "description": "Cada iorgute custa <strong>R$ 2,00</strong>. Complete a tabela com o valor correspondente à quantidade iorgutes.",
+                "table_values": [2, 4, 10, 14],
+                "table_values_answers": [4, 8, 20, 28],
+                "item_1": {
+                    "icon_default": "icon_iorgute",
+                    "name": "Iorgute",
+                    "unit": "un",
+                    "start_value": 1,
+                    "min_value": 1,
+                    "max_value": 20,
+                    "relation": 2
+                },
+                "item_2": {
+                    "icon_default": "icon_moeda",
+                    "name": "Moeda",
+                    "unit": "R$",
+                    "start_value": 2,
+                    "min_value": 1,
+                    "max_value": 30,
+                    "relation": 0.5
                 },
                 "stars": 1
             }
